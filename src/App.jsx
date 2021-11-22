da3fee3bac53451f17280cbb51187731739241fd
Javier Lucero <javimcl1986@gmail.com>
2021-11-22 13:40:57 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React, {useState} from "react";
+import React, { Fragment, useState, useRef } from "react";
 import { TodoList } from "./components/TodoList";
+import { v4 as uuidv4 } from "uuid";
 
-
-export function App(){
-    const [todos,setTodos] = useState([
-        {id: 1, task:"Tarea1", completed: fañse },
+export function App() {
+    const [todos, setTodos] = useState([
+        { id: 1, task: "Tarea1", completed: false },
     ]);
+
+    const todoTaskRef = useRef();
+
+    const handleTodoAdd = () => {
+        const task = todoTaskRef.current.value;
+        if(task=== '') return;
+        setTodos((prevTodos) => {
+            return [...prevTodos, {id: uuidv4(), task, complete: false}]
+        });
+
+    }
     return (
-    <TodoList todos={todos}/>
-    <input type="text" placeholder="Nueva Tarea" />
-    
+        <Fragment>
+            <TodoList todos={todos} />
+            <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea" />
+            <button onClick={handleTodoAdd}>Agregar</button>
+            <button>Eliminar</button>
+        </Fragment>
     );
-     
+
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { TodoItem } from './TodoItem';
 
 export function TodoList({ todos }) {
     return (
         <ul>
-            {todos.map((todo) =>
-                <li>Tarea</li>
-            )}
+            {todos.map((todo) =>(
+                <TodoItem todo={todo}/>
+            ))}
         </ul>
     );
 }
